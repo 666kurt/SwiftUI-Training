a3e9f662383d0167501c6bc081be3f29046db30d
Maxim <incentivedie@gmail.com>
2024-04-09 00:33:07 +0400

    initial commit

---
+++ SwiftUI-Training/SwiftUI_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TrainingApp.swift
+//  SwiftUI-Training
+//
+//  Created by Максим Шишлов on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
